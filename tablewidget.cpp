c15a0388457b3306e67c23fa0e81f8a4d2a4dabf
labyrinthofdreams <noemail@localhost.localhost>
2014-08-16 19:34:11 +0300

    Add Chrome drag 'n drop support for images

--- tablewidget.cpp
+++ tablewidget.cpp
@@ -1,12 +1,22 @@
 #include <QtCore>
 #include <QtGui>
+#include <QtNetwork/QNetworkAccessManager>
+#include <QtNetwork/QNetworkReply>
+#include <QtNetwork/QNetworkRequest>
 #include <QtWidgets>
 #include "tablewidget.hpp"
 
 TableWidget::TableWidget(QWidget *parent) :
     QTableWidget(parent),
-    config("CsvScreenshotFinderConfig.ini", QSettings::IniFormat)
+    config("CsvScreenshotFinderConfig.ini", QSettings::IniFormat),
+    netManager(nullptr),
+    droppedRow(0)
 {
+    netManager = new QNetworkAccessManager(this);
+
+    connect(netManager, SIGNAL(finished(QNetworkReply*)),
+            this,       SLOT(requestFinished(QNetworkReply*)));
+
     setAcceptDrops(true);
 }
 
@@ -17,12 +27,13 @@ QStringList TableWidget::mimeTypes() const
 
 bool TableWidget::dropMimeData(int row, int column, const QMimeData* data, Qt::DropAction action)
 {
+    Q_UNUSED(column);
+    Q_UNUSED(action);
+
     const auto urls = data->urls();
     if(urls.size() != 1) {
         return false;
     }
-
-    const QUrl& url = urls.first();
 
     const QDir outputDir(config.value("last_output_dir").toString());
     if(!outputDir.exists()) {
@@ -31,26 +42,52 @@ bool TableWidget::dropMimeData(int row, int column, const QMimeData* data, Qt::D
         return false;
     }
 
-    if(url.scheme() != "file") {
-        return false;
+    const QUrl& url = urls.first();
+    // Firefox gives a local file path while Chrome gives an http url
+    if(url.scheme() == "http") {
+        // Disable widget while downloading
+        setEnabled(false);
+        droppedRow = row;
+        netManager->get(QNetworkRequest(url));
     }
-
-    const QString imageName = QString("%1.jpg").arg(item(row, 2)->text());
-    const QString outputPath = outputDir.absoluteFilePath(imageName);
-    qDebug() << imageName << outputPath;
-
-    if(!QFile::exists(outputPath)) {
-        const QString imagePath = url.toLocalFile();
-        QImage image(imagePath);
-        image.save(outputPath, "JPEG", 100);
-
-        QImage thumb = image.scaledToWidth(150);
-
-        auto item = new QTableWidgetItem;
-        item->setData(Qt::DecorationRole, QPixmap::fromImage(thumb));
-        setItem(row, 0, item);
-        setRowHeight(row, thumb.height());
+    else {
+        const QString imageName = QString("%1.jpg").arg(item(row, 2)->text());
+        const QString outputPath = outputDir.absoluteFilePath(imageName);
+        if(!QFile::exists(outputPath)) {
+            const QString imagePath = url.toLocalFile();
+            QImage image(imagePath);
+            image.save(outputPath, "JPEG", 100);
+            setThumbnail(row, image.scaledToWidth(150));
+        }
     }
 
     return true;
+}
+
+void TableWidget::setThumbnail(int row, const QImage& thumb)
+{
+    auto item = new QTableWidgetItem;
+    item->setData(Qt::DecorationRole, QPixmap::fromImage(thumb));
+    setItem(row, 0, item);
+    setRowHeight(row, thumb.height());
+}
+
+void TableWidget::requestFinished(QNetworkReply* reply)
+{
+    QImage image;
+    image.loadFromData(reply->readAll());
+    if(image.isNull()) {
+        QMessageBox::critical(this, tr("Invalid image data"), tr("Not a valid image"));
+        return;
+    }
+
+    const QDir outputDir(config.value("last_output_dir").toString());
+    const QString imageName = QString("%1.jpg").arg(item(droppedRow, 2)->text());
+    const QString outputPath = outputDir.absoluteFilePath(imageName);
+    if(!QFile::exists(outputPath)) {
+        image.save(outputPath, "JPEG", 100);
+        setThumbnail(droppedRow, image.scaledToWidth(150));
+    }
+
+    setEnabled(true);
 }
--- tablewidget.hpp
+++ tablewidget.hpp
@@ -1,16 +1,17 @@
 #ifndef TABLEWIDGET_HPP
 #define TABLEWIDGET_HPP
 
-#include <QTableWidget>
 #include <QSettings>
+#include <QTableWidget>
 
+class QImage;
+class QNetworkAccessManager;
+class QNetworkReply;
 class QStringList;
 
 class TableWidget : public QTableWidget
 {
     Q_OBJECT
-
-    QSettings config;
 
 public:
     explicit TableWidget(QWidget *parent = 0);
@@ -22,6 +23,18 @@ public slots:
 protected:
     QStringList mimeTypes() const override;
     bool dropMimeData(int row, int column, const QMimeData *data, Qt::DropAction action) override;
+
+private:
+    QSettings config;
+
+    QNetworkAccessManager *netManager;
+
+    int droppedRow;
+
+    void setThumbnail(int row, const QImage& thumb);
+
+private slots:
+    void requestFinished(QNetworkReply *reply);
 };
 
 #endif // TABLEWIDGET_HPP
